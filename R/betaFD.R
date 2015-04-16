54c3798e590e4e21999f3bc86b0a3ccd0e973e1d
ibartomeus <ibartomeus@home>
2015-04-16 15:32:16 +0200

    Add functional and taxonimic diversity (pairwise)

---
+++ R/betaFD.R
@@ -0,0 +1,52 @@
+#' Functional betadiversity
+#' 
+#' Calculate functional trait beta diversity for two communities using Petchey and Gaston 2002 index 
+#' and Carvalho et al. 2012 decomposition.
+#' 
+#' @param S matrix or data frame of functional traits. Traits can be numeric, ordered, 
+#' or factor. NAs are tolerated. All species in c1 and c2 should be present with no extra species. 
+#' Species should be ordered alphabetically.\code{}
+#' @param c1 vector containing the species names in the first community. NA not tolerated. In addition, 
+#' the species labels in c1 and S must be identical. \code{}
+#' @param c2 vector containing the species names in the first community. NA not tolerated. In addition, 
+#' the species labels in c2 and S must be identical.\code{}
+#'
+#' @return Btot Total Betadiversity
+#' @return B_3 Beta diversity due to replacement
+#' @return Brich Beta diversity due to richness diferences
+#' @return quality the ouptput will print the quality of the dendogram representation.
+#' clustering  performance is assessed by the correlation with the cophenetic distance
+#' 
+#' 
+#' @export
+#' 
+#' @examples 
+#' ex1 <- betaFD(c1 = c("sp3", "sp2", "sp1", "sp4", "sp5"), c2 = c("sp6", "sp7", "sp8", "sp4", "sp5"), S = dummy$trait)
+#' ex1
+betaFD <- function(c1,c2,S, Tree = NULL){
+  B15 <- function(pm) with(pm,{(b+c)/(a+b+c)})
+  B_3  <-  function(pm) with(pm,{2*min(b,c)/(a+b+c)}) 
+  Brich  <-  function(pm) with(pm,{abs(b-c)/(a+b+c)})
+  b  <-  subset(c1, !c1 %in% c2)
+  c <- subset(c2, !c2 %in% c1)
+  a <- subset(c2, c2 %in% c1)
+  A_long <- data.frame(comm = c(rep("c1", length(c1)),
+                               rep("c2", length(c2)),
+                               rep("a", length(a)),
+                               rep("b", length(b)),
+                               rep("c", length(c))),
+                  species = c(c1,c2,a,b,c)) #note a,b or c can be not present if 0
+  A <- table(A_long$comm, A_long$species)
+  fd <- FD_dendro2(S, A, Tree, Cluster.method = "average", ord = "podani")
+  if(!is.na(fd$qual.FD[1])) print(paste("The quality of the dendogram is", round(fd$qual.FD[1], 2)))
+  branches <- list(a = fd[which(fd$comm == "a"), "FDpg"], 
+                   b = fd[which(fd$comm == "b"), "FDpg"], 
+                   c = fd[which(fd$comm == "c"), "FDpg"])
+  #recode non presences with 0's
+  branches[is.na(branches > 0)] <- 0 
+  beta_  <-  B15(branches)
+  beta_3  <-  B_3(branches)
+  beta_rich  <-  Brich(branches)
+  return(list(Btot = beta_, B_3 = beta_3, Brich = beta_rich)) 
+}
+
---
+++ R/betadiv.R
@@ -0,0 +1,31 @@
+#' Taxonimic betadiversity
+#' 
+#' Calculate taxonomic trait beta diversity for two communities using Carvalho et al. 2012 decomposition.
+#' 
+#' @param c1 vector containing the species names in the first community. NA not tolerated. \code{}
+#' @param c2 vector containing the species names in the first community. NA not tolerated. \code{}
+#'
+#' @return Btot Total Beta diversity
+#' @return B_3 Beta diversity due to replacement
+#' @return Brich Beta diversity due to richness diferences
+#' @return quality the ouptput will print the quality of the dendogram representation.
+#' clustering  performance is assessed by the correlation with the cophenetic distance
+#' 
+#' 
+#' @export
+#' 
+#' @examples 
+#' ex1 <- betadiv(c1 = c("sp3", "sp2", "sp1", "sp4", "sp5"), c2 = c("sp6", "sp7", "sp8", "sp4", "sp5"))
+#' ex1
+
+betadiv  <-  function(c1,c2){
+  B15 <- function(pm) with(pm,{(b+c)/(a+b+c)})
+  B_3  <-  function(pm) with(pm,{2*min(b,c)/(a+b+c)}) 
+  Brich  <-  function(pm) with(pm,{abs(b-c)/(a+b+c)})
+  pmb  <-  function(A,B) list(b=sum(!(A %in% B)),c=sum(!(B %in% A)),a=sum(B %in% A))
+  beta_  <-  B15(pmb(c1,c2))
+  beta_3  <-  B_3(pmb(c1,c2))
+  beta_rich  <-  Brich(pmb(c1,c2))
+  return(list(Btot = beta_, B_3 = beta_3, Brich = beta_rich))
+}
+
